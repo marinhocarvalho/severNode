350ecc1eeb6ca5401b043e2ec9247ed141509b44
marinhosrc907@gmail.com <marinhosrc907@gmail.com>
2016-09-15 21:10:23 -0400

    first commit

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var app = express();
+var path = require('path');
+app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, res){
 	//response.send('Hola Mundo desde NodeJS y Express');
 	res.sendFile(__dirname + '/public/index.html');
+})
+app.get('/pagina1',function(req, res){
+	res.sendFile(path.join(__dirname + '/public/pagina1.html'))
+})
+app.get('/pagina2',function(req, res){
+	res.sendFile(path.join(__dirname + '/public/pagina2.html'))
+})
+app.get('/user',function(req, res){
+	res.send({username:'Mario',password:'123456'});
 })
 
 app.listen(9090,function(){
